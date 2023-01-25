06fd8c1e5436194b0226e12384dfc3004de33e7c
Martin Tore <martintore@gmail.com>
2023-01-25 18:06:19 +0100

    added mixed enctype for file upload

--- controllers/auth.js
+++ controllers/auth.js
@@ -67,8 +67,7 @@ exports.getSignup = (req, res) => {
     title: "Create Account",
   });
 };
-
-exports.postSignup = (req, res, next) => {
+exports.postSignup = async (req, res, next) => {
   const validationErrors = [];
   if (!validator.isEmail(req.body.email))
     validationErrors.push({ msg: "Please enter a valid email address." });
@@ -87,57 +86,115 @@ exports.postSignup = (req, res, next) => {
     gmail_remove_dots: false,
   });
 
-  // const user = new User({
-  //   userName: req.body.userName,
-  //   email: req.body.email,
-  //   password: req.body.password,
-  //   avatar: req.body.avatar,
-  // });
-
   const user = new User({
     userName: req.body.userName,
     email: req.body.email,
     password: req.body.password,
   });
 
-  if (req.body.avatar == "upload") {
-  cloudinary.uploader.upload(req.file.path, (err, result) => {
-    if (err) {
+  if (req.body.avatar === 'upload') {
+    try {
+      const result = await cloudinary.uploader.upload(req.file.path);
+      user.avatar = result.secure_url;
+    } catch (err) {
       return next(err);
     }
-    const avatar = result.secure_url;
-    user.avatar = avatar;
-    checkExistingUser(user, req, res, next);
-  });
-} else {
-  user.avatar = req.body.avatar;
-  checkExistingUser(user, req, res, next);
-}
+  } else {
+    user.avatar = req.body.avatar;
+  }
 
-  function checkExistingUser(user, req, res, next) {
-    User.findOne(
-    { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
-    (err, existingUser) => {
+  try {
+    const existingUser = await User.findOne({
+      $or: [{ email: req.body.email }, { userName: req.body.userName }],
+    });
+    if (existingUser) {
+      req.flash("errors", {
+        msg: "Account with that email address or username already exists.",
+      });
+      return res.redirect("../signup");
+    }
+    await user.save();
+    req.logIn(user, (err) => {
       if (err) {
         return next(err);
       }
-      if (existingUser) {
-        req.flash("errors", {
-          msg: "Account with that email address or username already exists.",
-        });
-        return res.redirect("../signup");
-      }
-      user.save((err) => {
-        if (err) {
-          return next(err);
-        }
-        req.logIn(user, (err) => {
-          if (err) {
-            return next(err);
-          }
-          res.redirect("/profile");
-        });
-      });
-    }
-  );}
+      res.redirect("/profile");
+    });
+  } catch (err) {
+    return next(err);
+  }
 };
+
+// exports.postSignup = (req, res, next) => {
+//   const validationErrors = [];
+//   if (!validator.isEmail(req.body.email))
+//     validationErrors.push({ msg: "Please enter a valid email address." });
+//   if (!validator.isLength(req.body.password, { min: 8 }))
+//     validationErrors.push({
+//       msg: "Password must be at least 8 characters long",
+//     });
+//   if (req.body.password !== req.body.confirmPassword)
+//     validationErrors.push({ msg: "Passwords do not match" });
+
+//   if (validationErrors.length) {
+//     req.flash("errors", validationErrors);
+//     return res.redirect("../signup");
+//   }
+//   req.body.email = validator.normalizeEmail(req.body.email, {
+//     gmail_remove_dots: false,
+//   });
+
+//   // const user = new User({
+//   //   userName: req.body.userName,
+//   //   email: req.body.email,
+//   //   password: req.body.password,
+//   //   avatar: req.body.avatar,
+//   // });
+
+//   const user = new User({
+//     userName: req.body.userName,
+//     email: req.body.email,
+//     password: req.body.password,
+//   });
+
+//   if (req.file) {
+//   cloudinary.uploader.upload(req.file.path, (err, result) => {
+//     if (err) {
+//       return next(err);
+//     }
+//     const avatar = result.secure_url;
+//     user.avatar = avatar;
+//     checkExistingUser(user, req, res, next);
+//   });
+// } else {
+//   user.avatar = req.body.avatar;
+//   checkExistingUser(user, req, res, next);
+// }
+
+//   function checkExistingUser(user, req, res, next) {
+//     User.findOne(
+//     { $or: [{ email: req.body.email }, { userName: req.body.userName }] },
+//     (err, existingUser) => {
+//       if (err) {
+//         return next(err);
+//       }
+//       if (existingUser) {
+//         req.flash("errors", {
+//           msg: "Account with that email address or username already exists.",
+//         });
+//         return res.redirect("../signup");
+//       }
+//       user.save((err) => {
+//         if (err) {
+//           return next(err);
+//         }
+//         req.logIn(user, (err) => {
+//           if (err) {
+//             return next(err);
+//           }
+//           res.redirect("/profile");
+//         });
+//       });
+//     }
+//   );}
+// };
